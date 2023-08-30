08df100bf6c7fc272bbca7ff371a1d9318e34d52
Emil Ashirbekov <emkaashirbekov7910a@gmail.com>
2023-08-30 17:46:23 +0600

    almost adapted to different screen sizes

---
+++ frontend/src/pages/ServicesPage/ServicesPage.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "./Services.module.css";
+import Features from "../../components/Features/Features";
+import getInTouch from "../../assets/get-in-touch.svg";
+
+const Services: React.FC = () => {
+  return (
+    <>
+      <section className={styles.services}>
+        <div className={styles.services__container}>
+          <div className={styles.services__header}>
+            <h1 className={styles.services__title}>Services</h1>
+          </div>
+        </div>
+      </section>
+      <Features />
+      <div className={styles.getInTouch}>
+        <div className={styles.getInTouch__img}>
+          <img src={getInTouch} alt="get in touch img" />
+          <div className={styles.getInTouch__content}>
+            <h3>
+              Get in touch and <span>start</span> <span>growing</span> your
+              startup today
+            </h3>
+            <div className="flex">
+              <button className={styles.getInTouch__btn1}>Get in touch</button>
+              <button className={styles.getInTouch__btn2}>Get Started</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Services;
