55e2cb70fba731182cb6e606cf572788d1390d58
valentine <mokrousov.va@yandex.ru>
2023-08-30 15:54:15 +0600

    fixed manage.py

--- backend/content/models.py
+++ backend/content/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Contact(models.Model):
 
     email = models.EmailField(blank=True, null=True)
-    phone = models.CharField(blank=True, null=True)
+    phone = models.CharField(blank=True, null=True, max_length=12)
     address_ru = models.CharField(blank=True, null=True, max_length=255)
     address_en = models.CharField(blank=True, null=True, max_length=255)
     address_kg = models.CharField(blank=True, null=True, max_length=255)
