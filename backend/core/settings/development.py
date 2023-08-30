75990dd3d477612f2c59b9bf23403c5188301f00
grauverg <v.n.grauverg@gmail.com>
2023-08-30 11:55:51 +0600

    Splitted settings

---
+++ backend/core/settings/development.py
@@ -0,0 +1,12 @@
+from .base import BASE_DIR
+
+# DEBUG
+DEBUG = True
+
+# Database
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
---
+++ backend/core/settings/production.py
@@ -0,0 +1,16 @@
+from decouple import config
+
+# DEBUG
+DEBUG = False
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT')
+    }
+}
