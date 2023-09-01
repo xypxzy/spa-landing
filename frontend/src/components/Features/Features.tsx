312a762629e310d3e1ccc1c07d32a802c5e6eba8
Emil Ashirbekov <emkaashirbekov7910a@gmail.com>
2023-09-01 16:35:58 +0600

    added animations

--- frontend/src/components/Features/Features.tsx
+++ frontend/src/components/Features/Features.tsx
@@ -4,33 +4,66 @@ import FeaturesCard from './FeaturesCard';
 import { ReactComponent as ServicesIcon } from '../../assets/services-icon.svg';
 import { ReactComponent as ArrowLeft } from '../../assets/arrow-left.svg';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Features: React.FC = () => {
+  const featureAnimation = {
+    hidden: {
+      y: 100,
+      opacity: 0
+    },
+    visible: (custom: number) => ({
+      y: 0,
+      opacity: 1,
+      transition: { delay: custom * 0.2, ease: 'easeInOut' }
+    })
+  };
+
+  const animateText = {
+    hidden: {
+      y: 100,
+      opacity: 0
+    },
+    visible: (custom: number) => ({
+      y: 0,
+      opacity: 1,
+      transition: { delay: custom * 0.2 }
+    })
+  };
+
   return (
-    <section className={styles.features}>
+    <motion.section
+      className={styles.features}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ amount: 0.2 }}
+    >
       <div className={styles.features__container}>
         <div className={styles.features__header}>
           <div className={styles.features__left}>
             <ServicesIcon className=" h-20 w-20 mb-10" />
-            <h2>
+            <motion.h2 custom={1} variants={animateText}>
               High-impact services to take your business to the next level
-            </h2>
+            </motion.h2>
           </div>
           <Link className={styles.features__btn} to="servies">
             <p>Our services</p>
             <ArrowLeft />
           </Link>
         </div>
-        <div className={styles.features__cards}>
+        <motion.div
+          variants={featureAnimation}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ amount: 0.2 }}
+          className={styles.features__cards}
+        >
           <FeaturesCard />
           <FeaturesCard />
           <FeaturesCard />
-          <FeaturesCard />
-          <FeaturesCard />
-          <FeaturesCard />
-        </div>
+        </motion.div>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
--- frontend/src/pages/ServicesPage/ServicesPage.tsx
+++ frontend/src/pages/ServicesPage/ServicesPage.tsx
@@ -4,10 +4,37 @@ import Features from '../../components/Features/Features';
 import serviceImg from '../../assets/serviceImg.png';
 import marketingLogo from '../../assets/marketing-logo.png';
 import { ReactComponent as DoneIcon } from '../../assets/marketing-icon.svg';
+import { motion } from 'framer-motion';
 
 const Services: React.FC = () => {
+  const featureAnimation = {
+    hidden: {
+      y: 100,
+      opacity: 0
+    },
+    visible: (custom: number) => ({
+      y: 0,
+      opacity: 1,
+      transition: { delay: custom * 0.2, ease: 'easeInOut' }
+    })
+  };
+  const animateText = {
+    hidden: {
+      y: 100,
+      opacity: 0
+    },
+    visible: (custom: number) => ({
+      y: 0,
+      opacity: 1,
+      transition: { delay: custom * 0.2 }
+    })
+  };
   return (
-    <>
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ amount: 0.2 }}
+    >
       <section className={styles.services}>
         <div className={styles.services__container}>
           <div className={styles.services__header}>
@@ -16,50 +43,82 @@ const Services: React.FC = () => {
         </div>
       </section>
       <Features />
-      <section className={styles.marketing}>
+      <motion.section
+        className={styles.marketing}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ amount: 0.2 }}
+      >
         <div className={styles.marketing__container}>
           <div className={styles.marketing__content}>
             <img src={serviceImg} alt="PC" className={styles.marketing__img} />
             <div className={styles.marketing__right}>
               <div className={styles.marketing__top}>
-                <img src={marketingLogo} alt="marketing logo" />
-                <h3 className={styles.marketing__title}>
+                <motion.img
+                  variants={featureAnimation}
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ amount: 0.2 }}
+                  src={marketingLogo}
+                  alt="marketing logo"
+                />
+                <motion.h3
+                  custom={1}
+                  variants={animateText}
+                  className={styles.marketing__title}
+                >
                   We take your business reach from thousands to millions
-                </h3>
+                </motion.h3>
               </div>
               <hr className={styles.hr} />
               <div className={styles.marketing__middle}>
-                <p className={styles.marketing__desc}>
+                <motion.p
+                  custom={1}
+                  variants={animateText}
+                  className={styles.marketing__desc}
+                >
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit
                   sollicitudinbibendum senectus scelerisque non. Turpis matis
                   morbi vivera ipsum adipiscing Mauris volutpat sagittis, sit
-                </p>
+                </motion.p>
                 <ul className={styles.marketing__list}>
-                  <li className={styles.marketing__item}>
+                  <motion.li
+                    custom={1}
+                    variants={animateText}
+                    className={styles.marketing__item}
+                  >
                     <span>
                       <DoneIcon />
                     </span>
                     Dolor duis lorem enim eu turpis potenti nulla semper
-                  </li>
-                  <li className={styles.marketing__item}>
+                  </motion.li>
+                  <motion.li
+                    custom={1}
+                    variants={animateText}
+                    className={styles.marketing__item}
+                  >
                     <span>
                       <DoneIcon />
                     </span>
                     Dolor duis lorem enim eu turpis potenti nulla semper
-                  </li>
-                  <li className={styles.marketing__item}>
+                  </motion.li>
+                  <motion.li
+                    custom={1}
+                    variants={animateText}
+                    className={styles.marketing__item}
+                  >
                     <span>
                       <DoneIcon />
                     </span>
                     Dolor duis lorem enim eu turpis potenti nulla semper
-                  </li>
+                  </motion.li>
                 </ul>
               </div>
             </div>
           </div>
         </div>
-      </section>
-    </>
+      </motion.section>
+    </motion.div>
   );
 };
 
