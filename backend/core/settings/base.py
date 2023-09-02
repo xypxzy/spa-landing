5921eaf66a81c1ad23eb16fd8a9698f7bc1c4871
grauverg <v.n.grauverg@gmail.com>
2023-09-02 15:15:08 +0600

    Added CORS

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from decouple import config
 from django.utils.translation import gettext_lazy
 
+from .cors import *
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
@@ -21,6 +23,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 # Application definition
 THIRD_PARTY_APPS = [
     'rest_framework',
+    'corsheaders',
 ]
 
 LOCAL_APPS = [
@@ -46,6 +49,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -90,6 +94,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
 
 
 # Internationalization
@@ -142,7 +147,7 @@ JAZZMIN_SETTINGS = {
         {"app": "content"},
     ],
 
-        # Whether to display the side menu
+    # Whether to display the side menu
     "show_sidebar": True,
 
     # Whether to aut expand the menu
@@ -171,3 +176,5 @@ JAZZMIN_SETTINGS = {
 
     # "language_chooser": True
 }
+
+from .production import DATABASES
