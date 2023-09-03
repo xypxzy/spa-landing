6ac7704f0bf61ef0aefc0d5496adc7ad9ebbc118
Emil Ashirbekov <emkaashirbekov7910a@gmail.com>
2023-09-03 16:51:01 +0600

    .

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       backgroundImage: {
-        'hero-pattern': "url('./src/assets/BannerImage.png')",
+        'hero-pattern': "url('./src/assets/BannerImage.png')"
       }
-    },
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
